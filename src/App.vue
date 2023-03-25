5893f9a68a374a99a2f3f694d2e1341106051f47
sean-code <john.nganga@student.moringaschool.com>
2023-03-25 13:28:46 +0300

    Update: match the outlook with the prior

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,58 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <navbar
+    :pages="pages"
+    :active-page="activePage"
+    :nav-link-click="(index) => activePage = index"
+    >
+  </navbar>
+  <page-viewer
+    :page="pages[activePage]">
+  </page-viewer>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PageViewer from './components/PageViewer.vue';
+import Navbar from './components/Navbar.vue';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    components:{
+        Navbar,
+        PageViewer
+    },
+        data() {
+                return {
+                    activePage: 0,
+                    theme: 'dark',
+                    pages: [
+                        {
+                            link: {text: "Home", url: 'index.html'},
+                            pageTitle: 'Home page!',
+                            paragraph: 'This is the Home page of Vue'
+                        },
+                        {
+                            link: {text: "About", url: 'about.html'},
+                            pageTitle: 'About page!',
+                            paragraph: 'This is the About Page of Vue'
+                        },
+                        {
+                            link: {text: "Contact", url: 'contact.html'},
+                            pageTitle: 'Contact page!',
+                            paragraph: 'This is the Contact Page'
+                        }
+                    ],
+                };
+            },
+
 }
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,54 @@
+<template>
+    <nav 
+        :class="[`navbar-${theme}`, `bg-${theme}`, 'navbar', 'navbar-expand-lg']">
+        <a class="navbar-brand" href="#">Vue Project</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarText">
+          <ul class="navbar-nav mr-auto">
+            <li v-for="(page, index) in pages" class="nav-item" :key="index">
+                <a
+                    class="nav-link "
+                    :class="{ active: activePage == index }"
+                    aria-current="page"
+                    :href="page.link.url"
+                    :title="`This Link Goes To ${page.text}`"
+                    @click.prevent="navLinkClick(index)"
+                    >{{ page.link.text }}</a>
+              </li>
+          </ul>
+          <form class="d-flex">
+            <button class="btn btn-primary"
+            @click.prevent="changeTheme()"
+            >Toggle
+            </button>
+          </form>
+        </div>
+      </nav>
+</template>
+
+<script>
+    export default{
+        props: ['pages', 'activePage', 'navLinkClick'],
+        data() {
+            return{
+                theme: 'light',
+            }
+
+        },
+        methods: {
+                    changeTheme() {
+                        this.theme = this.theme === 'light' ? 'dark' : 'light';
+                        }
+                }
+
+    }
+
+</script>
+
+
+
+<style>
+
+</style>
